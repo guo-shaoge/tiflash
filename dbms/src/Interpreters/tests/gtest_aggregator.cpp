fcf5f1c9f9500aa1fb9d4396c97b429d1b44b718
guo-shaoge <shaoge1994@163.com>
2024-03-20 10:28:09 +0800

    gtest_aggregator 256
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Interpreters/tests/gtest_aggregator.cpp
+++ dbms/src/Interpreters/tests/gtest_aggregator.cpp
@@ -161,7 +161,7 @@ public:
     const std::string col2_name = "col2_decimal128";
     const size_t rows_per_block = 4096;
     const size_t total_rows = 40000000;
-    const size_t prec = 15;
+    const size_t prec = 19;
     const size_t scale = 6;
 
     DataTypePtr col1_data_type;
@@ -239,7 +239,7 @@ try
     auto res_col1 = res_block.getByPosition(0).column;
     auto res_col2 = res_block.getByPosition(1).column;
     const auto * res_col1_decimal128 = checkAndGetColumn<ColumnDecimal<Decimal128>>(res_col1.get());
-    const auto * res_col2_decimal128 = checkAndGetColumn<ColumnDecimal<Decimal128>>(res_col2.get());
+    const auto * res_col2_decimal256 = checkAndGetColumn<ColumnDecimal<Decimal256>>(res_col2.get());
     for (size_t i = 0; i < res_block.rows(); ++i)
     {
         Field field1;
@@ -247,9 +247,9 @@ try
         DecimalField field1_decimal(Decimal128(field1.get<Decimal128::NativeType>()), res_col1_decimal128->getScale());
 
         Field field2;
-        res_col2_decimal128->get(i, field2);
+        res_col2_decimal256->get(i, field2);
         // todo how to get field not seeing column structure?
-        DecimalField field2_decimal(Decimal128(field2.get<Decimal128::NativeType>()), res_col2_decimal128->getScale());
+        DecimalField field2_decimal(Decimal256(field2.get<Decimal256::NativeType>()), res_col2_decimal256->getScale());
 
         LOG_DEBUG(log, "gjt debug col {}, {}, {}", i, field1_decimal.toString(), field2_decimal.toString());
     }
