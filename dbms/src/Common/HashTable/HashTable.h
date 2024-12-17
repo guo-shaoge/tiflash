a94693794ca6980717970b7acac9a2b01f77063d
guo-shaoge <shaoge1994@163.com>
2024-12-17 17:20:11 +0800

    precomputed grower
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/Common/HashTable/HashTable.h
+++ dbms/src/Common/HashTable/HashTable.h
@@ -254,6 +254,20 @@ struct HashTableGrower
     /// The state of this structure is enough to get the buffer size of the hash table.
 
     UInt8 size_degree = initial_size_degree;
+    size_t precalculated_mask = (1ULL << initial_size_degree) - 1;
+    size_t precalculated_max_fill = 1ULL << (initial_size_degree - 1);
+    static constexpr size_t max_size_degree = 23;
+
+public:
+    UInt8 sizeDegree() const { return size_degree; }
+
+    void increaseSizeDegree(UInt8 delta)
+    {
+        size_degree += delta;
+        precalculated_mask = (1ULL << size_degree) - 1;
+        precalculated_max_fill = 1ULL << (size_degree - 1);
+    }
+
     static constexpr auto initial_count = 1ULL << initial_size_degree;
 
     /// If collision resolution chains are contiguous, we can implement erase operation by moving the elements.
@@ -262,35 +276,35 @@ struct HashTableGrower
     /// The size of the hash table in the cells.
     size_t bufSize() const { return 1ULL << size_degree; }
 
-    size_t maxFill() const { return 1ULL << (size_degree - 1); }
-    size_t mask() const { return bufSize() - 1; }
-
     /// From the hash value, get the cell number in the hash table.
-    size_t place(size_t x) const { return x & mask(); }
+    size_t place(size_t x) const { return x & precalculated_mask; }
 
     /// The next cell in the collision resolution chain.
-    size_t next(size_t pos) const
-    {
-        ++pos;
-        return pos & mask();
-    }
+    size_t next(size_t pos) const { return (pos + 1) & precalculated_mask; }
 
     /// Whether the hash table is sufficiently full. You need to increase the size of the hash table, or remove something unnecessary from it.
-    bool overflow(size_t elems) const { return elems > maxFill(); }
+    bool overflow(size_t elems) const { return elems > precalculated_max_fill; }
 
     /// Increase the size of the hash table.
-    void increaseSize() { size_degree += size_degree >= 23 ? 1 : 2; }
+    void increaseSize() { increaseSizeDegree(size_degree >= max_size_degree ? 1 : 2); }
 
     /// Set the buffer size by the number of elements in the hash table. Used when deserializing a hash table.
     void set(size_t num_elems)
     {
-        size_degree = num_elems <= 1 ? initial_size_degree
-                                     : ((initial_size_degree > static_cast<size_t>(log2(num_elems - 1)) + 2)
-                                            ? initial_size_degree
-                                            : (static_cast<size_t>(log2(num_elems - 1)) + 2));
+        if (num_elems <= 1)
+            size_degree = initial_size_degree;
+        else if (initial_size_degree > static_cast<size_t>(log2(num_elems - 1)) + 2)
+            size_degree = initial_size_degree;
+        else
+            size_degree = static_cast<size_t>(log2(num_elems - 1)) + 2;
+        increaseSizeDegree(0);
     }
 
-    void setBufSize(size_t buf_size_) { size_degree = static_cast<size_t>(log2(buf_size_ - 1) + 1); }
+    void setBufSize(size_t buf_size_)
+    {
+        size_degree = static_cast<size_t>(log2(buf_size_ - 1) + 1);
+        increaseSizeDegree(0);
+    }
 };
 
 
