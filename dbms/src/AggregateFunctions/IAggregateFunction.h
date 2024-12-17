4521fc63eb24429258273739edb49d08ed55444e
guo-shaoge <shaoge1994@163.com>
2024-12-17 18:02:44 +0800

    prefetch addBatch
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- dbms/src/AggregateFunctions/IAggregateFunction.h
+++ dbms/src/AggregateFunctions/IAggregateFunction.h
@@ -235,20 +235,33 @@ public:
         Arena * arena,
         ssize_t if_argument_pos = -1) const override
     {
+        const auto end = start_offset + batch_size;
         if (if_argument_pos >= 0)
         {
             const auto & flags = assert_cast<const ColumnUInt8 &>(*columns[if_argument_pos]).getData();
-            for (size_t i = start_offset; i < start_offset + batch_size; ++i)
+            for (size_t i = start_offset; i < end; ++i)
             {
+                const auto place_idx = i - start_offset;
+                const auto prefetch_idx = place_idx + 16;
+                if likely (prefetch_idx < end)
+                    __builtin_prefetch(places[prefetch_idx]);
+
                 if (flags[i] && places[i - start_offset])
-                    static_cast<const Derived *>(this)->add(places[i - start_offset] + place_offset, columns, i, arena);
+                    static_cast<const Derived *>(this)->add(places[place_idx] + place_offset, columns, i, arena);
             }
         }
         else
         {
-            for (size_t i = start_offset; i < start_offset + batch_size; ++i)
+            for (size_t i = start_offset; i < end; ++i)
+            {
+                const auto place_idx = i - start_offset;
+                const auto prefetch_idx = place_idx + 16;
+                if likely (prefetch_idx < end)
+                    __builtin_prefetch(places[prefetch_idx]);
+
                 if (places[i - start_offset])
-                    static_cast<const Derived *>(this)->add(places[i - start_offset] + place_offset, columns, i, arena);
+                    static_cast<const Derived *>(this)->add(places[place_idx] + place_offset, columns, i, arena);
+            }
         }
     }
 
